f284aa61406280a0bbdb9ad57ffa1c72847fd82a
jacob <jacobpham95@gmail.com>
2022-12-16 19:56:36 +1100

    fixed dailypost function

--- discord_bot.py
+++ discord_bot.py
@@ -1,32 +1,33 @@
 import discord
 import os
-from discord.ext import commands#, tasks
+from discord.ext import commands
 from discord.ext.commands import Bot
-from datetime import datetime, time#, timedelta
+from datetime import datetime, time
 import asyncio
 
-##prefix to be entered before commands i.e. !test
+#set bot params and defines bot variable, ! as bot prefix, disables default help command, sets intents to default
 intents = discord.Intents.default()
-bot = Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents, help_command=None) ##sets ! prefix and removes default help command
-WHEN = time(4, 30, 00) #time to trigger qotd
+bot = Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents, help_command=None)
 
 
-##reports to console when bot is primed and ready
+#reports to console when bot is primed and ready
 @bot.event
 async def on_ready() -> None:
   print(f"Logged in as {bot.user.name}")
   print("-----------------")
 
-async def check_time():
+#function for a while True loop that posts 'working' to the general at 5:30pm daily
+async def dailypost():
     await bot.wait_until_ready()
     while True:
-        general = bot.get_channel(985030405902205000)
+        general = bot.get_channel("channel no. here")
         current_time = datetime.utcnow()
-        if current_time.hour < 17: #and current_time.minute == 30:
+        if current_time.hour < 17 and current_time.minute == 30:
           await general.send('working')
         await asyncio.sleep(60)
 
-bot.loop.create_task(check_time())
+#calls previous function as a loop
+bot.loop.create_task(dailypost())
 
 #Connects discord websocket and handles too many requests error
 try:
